42de2225dfbd8dbf0b1aba591c80f03b53b9f0be
OxygenTheSmolAngel <ztwo5548@gmail.com>
2025-06-22 09:25:55 -0400

    Replaced the burnt Celebi and Dead Gold front sprites with new ones made by me.

--- gfx/pokemon/celebi_lost/anim.asm
+++ gfx/pokemon/celebi_lost/anim.asm
@@ -1,16 +1,18 @@
 	frame 0, 10
 	setrepeat 2
-	frame 1, 08
+	frame 5, 08
 	frame 0, 08
 	dorepeat 2
-	frame 0, 04
-	frame 3, 06
-	frame 4, 06
-	frame 5, 07
-	frame 6, 14
-	frame 6, 12
-	frame 5, 07
-	frame 4, 06
+	frame 0, 06
+	frame 1, 08
+	frame 2, 06
 	frame 3, 04
+	frame 4, 10
+	frame 3, 08
+	frame 2, 10
+	frame 4, 10
+	frame 3, 08
+	frame 2, 08
+	frame 1, 08
 	frame 0, 16
 	endanim
